84301e993863a04b9b784b327d18e7ce3e41b96f
Deepika Upadhyay <deepika@koor.tech>
2022-09-20 20:01:01 +0530

    ci: amend workflow to shift docker img push to dockerhub in shell script
    Signed-off-by: Deepika Upadhyay <deepika@koor.tech>

--- src/build-push-ceph-container-imgs-base.sh
+++ src/build-push-ceph-container-imgs-base.sh
@@ -8,5 +8,7 @@ CEPH_POINT_RELEASE=${CEPH_POINT_RELEASE:=v17.2.3}
 #  --build-arg CEPH_POINT_RELEASE="17.2.3" \
 #  . --file Dockerfile \
 #  --tag koor/ceph-container-unofficial:$latest_ceph_image_tag
-docker build --build-arg CEPH_POINT_RELEASE="${CEPH_POINT_RELEASE}" . --file Dockerfile --tag dexter816/temp-repo:v17
+docker build --build-arg CEPH_POINT_RELEASE="${CEPH_POINT_RELEASE}" . --file Dockerfile --tag dexter816/temp-repo:${CEPH_POINT_RELEASE}
+docker push dexter816/temp-repo:${CEPH_POINT_RELEASE}
+
 
