18eed85018deb017d7d188fefd146afd5becaa3b
Deepika Upadhyay <deepika@koor.tech>
2022-09-20 16:43:09 +0530

    src: rename CEPH_IMAGE_TAG to CEPH_POINT_RELEASE used by ceph-container project
    Signed-off-by: Deepika Upadhyay <deepika@koor.tech>

--- src/build-push-ceph-container-imgs-base.sh
+++ src/build-push-ceph-container-imgs-base.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-CEPH_IMAGE_TAG=${CEPH_IMAGE_TAG:=v17.2.3}
+CEPH_POINT_RELEASE=${CEPH_POINT_RELEASE:=v17.2.3}
 
 # docker build \
-#  --build-arg CEPH_IMAGE_TAG="17.2.3" \
+#  --build-arg CEPH_POINT_RELEASE="17.2.3" \
 #  . --file Dockerfile \
 #  --tag koor/ceph-container-unofficial:$latest_ceph_image_tag
-docker build --build-arg CEPH_IMAGE_TAG="${CEPH_IMAGE_TAG}" . --file Dockerfile --tag dexter816/temp-repo:v17
+docker build --build-arg CEPH_POINT_RELEASE="${CEPH_POINT_RELEASE}" . --file Dockerfile --tag dexter816/temp-repo:v17
 
--- src/build-push-ceph-container-imgs-pacific.sh
+++ src/build-push-ceph-container-imgs-pacific.sh
@@ -3,12 +3,12 @@
 set -ex
 
 VERSION_REGEX="v16"
-CEPH_IMAGE_TAG=$(curl -s -L \
+CEPH_POINT_RELEASE=$(curl -s -L \
                  https://quay.io/api/v1/repository/ceph/ceph/tag\?page_size\=100 \
 	         | jq '."tags"[].name' \
 		 | sort -r --version-sort \
 		 | grep $VERSION_REGEX \
 		 | head -n 1)
 
-CEPH_IMAGE_TAG=$CEPH_IMAGE_TAG ./build-push-ceph-container-imgs-base.sh
+CEPH_POINT_RELEASE=$CEPH_POINT_RELEASE ./build-push-ceph-container-imgs-base.sh
 
