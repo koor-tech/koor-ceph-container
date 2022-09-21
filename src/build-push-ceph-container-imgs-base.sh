611ab35a2c6e50a56235da58aba98724cf1d153a
Deepika Upadhyay <deepika@koor.tech>
2022-09-21 17:14:23 +0530

    src: add dir path to build-push-ceph-container scripts
    for generic run from anywhere support
    
    Signed-off-by: Deepika Upadhyay <deepika@koor.tech>

--- src/build-push-ceph-container-imgs-base.sh
+++ src/build-push-ceph-container-imgs-base.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
 set -ex
+script="$0"
+basename="$(dirname $0)"
 
 CEPH_POINT_RELEASE=${CEPH_POINT_RELEASE:=v17.2.3}
 
@@ -8,7 +10,7 @@ CEPH_POINT_RELEASE=${CEPH_POINT_RELEASE:=v17.2.3}
 #  --build-arg CEPH_POINT_RELEASE="17.2.3" \
 #  . --file Dockerfile \
 #  --tag koor/ceph-container-unofficial:$latest_ceph_image_tag
-docker build --build-arg CEPH_POINT_RELEASE="${CEPH_POINT_RELEASE}" . --file Dockerfile --tag dexter816/temp-repo:${CEPH_POINT_RELEASE}
+docker build --build-arg CEPH_POINT_RELEASE=${CEPH_POINT_RELEASE} . --file $basename/Dockerfile --tag dexter816/temp-repo:${CEPH_POINT_RELEASE}
 docker push dexter816/temp-repo:${CEPH_POINT_RELEASE}
 
 
