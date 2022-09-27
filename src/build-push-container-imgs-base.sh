61abdacdab09c56d16e2763aa98d9afa587c4bee
Deepika Upadhyay <deepika@koor.tech>
2022-09-27 17:02:43 +0530

    src: fail if CEPH_POINT_RELEASE not provided
    - instead of using hardcoded version that might need maintainence, fail
    the build-push-container-imgs-base.sh script if CEPH_POINT_RELEASE is
    not provided during script run.
    
    - Dockerfile refactor
    
    Signed-off-by: Deepika Upadhyay <deepika@koor.tech>

--- src/build-push-container-imgs-base.sh
+++ src/build-push-container-imgs-base.sh
@@ -4,12 +4,11 @@ set -ex
 script="$0"
 basename="$(dirname $0)"
 
-CEPH_POINT_RELEASE=${CEPH_POINT_RELEASE:=v17.2.3}
+if [ -z "$CEPH_POINT_RELEASE" ]; then
+  echo "no CEPH_POINT_RELEASE provided... "
+  exit 1
+fi
 
-# docker build \
-#  --build-arg CEPH_POINT_RELEASE="17.2.3" \
-#  . --file Dockerfile \
-#  --tag koor/ceph-container-unofficial:$latest_ceph_image_tag
 docker build --build-arg CEPH_POINT_RELEASE=${CEPH_POINT_RELEASE} . --file $basename/Dockerfile --tag ${CONTAINER_REGISTRY}/${CONTAINER_REPO}:${CEPH_POINT_RELEASE}
 docker push ${CONTAINER_REGISTRY}/${CONTAINER_REPO}:${CEPH_POINT_RELEASE}
 
